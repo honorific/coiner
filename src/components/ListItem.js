a5d65b4938ac67879eaf1ccd747d60bafbeac8ec
honorific <honorific.info@gmail.com>
2023-05-03 13:23:38 +0430

    styleing the list

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,8 +2,17 @@ import {Link} from 'react-router-dom'
 
 const ListItem = ({coin}) => {
   return (
-    <div>
-      <Link to={`/${coin.id}`}>{coin.name}</Link>
+    <div className='home-crypto'>
+      <Link to={`/${coin.id}`}>
+        <span className='home-crypto-image'>
+          <img src={coin.image} />
+        </span>
+        <span className='home-crypto-name'>{coin.name}</span>
+        <span className='home-crypto-prices'>
+          <span>{coin.priceBTC} BTC</span>
+          <span>{coin.priceUsd} USD</span>
+        </span>
+      </Link>
     </div>
   )
 }
